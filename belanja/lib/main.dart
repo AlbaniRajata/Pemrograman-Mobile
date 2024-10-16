5151ecc8e9d3eba6a45df3d055f8dfcd34a211aa
Albani Rajata Malik <albeebro002@gmail.com>
2024-10-16 23:01:41 +0700

    praktikum 5 - code lab 6

--- belanja/lib/main.dart
+++ belanja/lib/main.dart
@@ -1,7 +1,15 @@
+import 'package:belanja/pages/home_page.dart';
+import 'package:belanja/pages/item_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MaterialApp(
+    initialRoute: '/',
+    routes: {
+      '/': (context) => HomePage(),
+      '/item': (context) => ItemPage(),
+    },
+  ));
 }
 
 class MainApp extends StatelessWidget {
@@ -10,6 +18,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Center(
           child: Text('Hello World!'),
